0fa33a6c129e50cf7df50300499df18ae36002e6
pritiikumari <101092380+pritiikumari@users.noreply.github.com>
2022-08-19 11:25:43 +0530

    Add files via upload

--- StreamCollectorsExercise.java
+++ StreamCollectorsExercise.java
@@ -1,16 +1,23 @@
-package StreamOperations;
+package Stream;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
 
 public class StreamCollectorsExercise {
 	public static void main(String[] args) {
 		List<Player> player = Arrays.asList(new Player("Sachin",400,20000,400,"India"),new Player("Raina",423,21000,420,"England"),new Player("Kohli",425,22000,425,"India"),new Player("Rohit",300,15000,500,"India"),new Player("Dhoni",200,12000,250,"India"));
 		getPlayersByCountry(player);
+		getPlayerNamesByCountry(player);
 		getTotalPlayersByCountry(player);
 		getTotalRunsByCountry(player);
+		getMaxScoreByCountry(player);
+		getPlayerNamesStringByCountry(player);
 	
 	
 	
@@ -31,10 +38,17 @@ public class StreamCollectorsExercise {
 		 System.out.println(n3);
 	}
 	public static void getMaxScoreByCountry(List<Player> p) {
-		//Map<String, Integer>  n4 = p.stream().collect(Collectors.groupingBy(k -> k.getCountry() , Collectors.max(Comparator.comparing(Player::getRuns()).get()));
-		 //System.out.println(n4);
-		
-	
 
-}
+		Map<Object, Object> highest = p.stream()
+        .collect(Collectors.groupingBy(Player::getCountry, Collectors.groupingBy(Player::getRuns,TreeMap::new, Collectors.toList())))
+        .entrySet().stream()
+        .collect(Collectors.toMap(e -> e.getKey(),
+                e -> e.getValue().lastEntry().getKey()));
+		System.out.println(highest);
+	}
+	public static void getPlayerNamesStringByCountry(List<Player> p){
+		Map<Object, Object> countryNameWithPlayerName = p.stream().collect(Collectors.toMap(p1->p1.getCountry(),p1->p1.getPlayerName(), (s, a) -> s + ", " + a, LinkedHashMap::new));
+		System.out.println(countryNameWithPlayerName);
+		}
+	
 }
